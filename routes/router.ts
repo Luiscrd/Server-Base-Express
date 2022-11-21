284be027c28eeb7e3eb6528ed02063c66076bdb4
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-21 07:09:50 +0100

    Rutas Basica

--- routes/router.ts
+++ routes/router.ts
@@ -6,25 +6,90 @@ const router = Router();
 
 console.log(colors.green(`[Routes/] Messages: ${BASE_URL}:${SERVER_PORT}/messages`));
 
+// GET Basic request
 router.get('/messages', (req: Request, res: Response) => {
 
-    res.status(200).json({
-        ok: true,
-        msg: 'Eeverything is fine - GET'
-    });
+    try {
 
-    console.log(colors.green('[Routes/] GET: Response ok from /messages'));
+        res.status(200).json({
+            ok: true,
+            msg: 'Eeverything is fine - GET'
+        });
+    
+        console.log(colors.green('[Routes/] GET: Response ok from /messages'));
+        
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Server Error - GET'
+        });
+    
+        console.log(colors.red('[Routes/] GET: Error from /messages'));
+        console.log(colors.red(`[Routes/] ${error}`));
+
+    }
 
 });
 
+// POST With elements in the body
 router.post('/messages', (req: Request, res: Response) => {
 
-    res.status(200).json({
-        ok: true,
-        msg: 'Eeverything is fine - POST'
-    });
+    const body = req.body.body;
 
-    console.log(colors.green('[Routes/] POST: Response ok from /messages'));
+    const to = req.body.to;
+
+    try {
+
+        res.status(200).json({
+            ok: true,
+            msg: 'Eeverything is fine - POST',
+            body,
+            to
+        });
+    
+        console.log(colors.green('[Routes/] POST: Response ok from /messages'));
+        
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Server Error - POST'
+        });
+    
+        console.log(colors.red('[Routes/] POST: Error from /messages'));
+        console.log(colors.red(`[Routes/] ${error}`));
+        
+    }
+
+});
+
+// POST With elements in the url
+router.post('/messages/:id', (req: Request, res: Response) => {
+
+    const id = req.params.id;
+
+    try {
+
+        res.status(200).json({
+            ok: true,
+            msg: 'Eeverything is fine - POST',
+            id
+        });
+    
+        console.log(colors.green('[Routes/] POST: Response ok from /messages'));
+        
+    } catch (error) {
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Server Error - POST'
+        });
+    
+        console.log(colors.red('[Routes/] POST: Error from /messages'));
+        console.log(colors.red(`[Routes/] ${error}`));
+        
+    }
 
 });
 
